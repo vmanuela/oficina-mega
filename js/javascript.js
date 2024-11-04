8b4be4aed7abc3a45f14eefb63be3f550b8f8efc
Manuela Vicente <manueladevv@gmail.com>
2024-11-04 11:52:49 -0300

    Primeiro commit

---
+++ js/javascript.js
@@ -0,0 +1,50 @@
+// Turns the menu hamburguer into a X  //
+
+const menuHamburguer = document.querySelector('.menu-hamburguer')
+menuHamburguer.addEventListener('click', () => {
+    toggleMenu();
+});
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuHamburguer.classList.toggle('change');
+
+    if (menuHamburguer.classList.contains('change')) {
+        nav.style.display = 'block'
+
+    }else {
+        nav.style.display = 'none'
+    }
+
+}
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    
+    const offset = -currentSlide * 100;
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Mudar a imagem automaticamente a cada 3 segundos
+setInterval(nextSlide, 3000);
+
+// Iniciar mostrando o primeiro slide
+showSlide(currentSlide);
+
